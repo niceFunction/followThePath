91201d44c2cd87b2d5aae317d0aacd10c86c7f8b
Johan Lindberg <jayelindberg@gmail.com>
2019-08-27 20:38:47 +0200

    Tile spawning

---
+++ follow_The_Path/followThePath/Assets/Scripts/Tile.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    [SerializeField] private float length = 19.9f;
+
+    [SerializeField] private Tile[] connectsTo;
+
+    public Tile SpawnNext()
+    {
+        Tile t = connectsTo[Random.Range(0, connectsTo.Length)];
+        t = Instantiate(t.gameObject).GetComponent<Tile>();
+        t.transform.position = this.transform.position + new Vector3(0f, 0f, length);
+
+        return t;
+    }
+}
--- follow_The_Path/followThePath/Assets/Scripts/scr_TileManager.cs
+++ follow_The_Path/followThePath/Assets/Scripts/scr_TileManager.cs
@@ -4,17 +4,26 @@ using UnityEngine;
 
 public class scr_TileManager : MonoBehaviour
 {
+    [SerializeField] private Tile startTile;
 
-    public GameObject[] leftTilePrefabs;
-    public GameObject[] midTilePrefabs;
-    public GameObject[] rightTilePrefabs;
+    // Serialized private fields, because we don't want other objects to access these
+    [SerializeField] private GameObject[] leftTilePrefabs;
+    [SerializeField] private GameObject[] midTilePrefabs;
+    [SerializeField] private GameObject[] rightTilePrefabs;
 
-    private float tileLength = 19.9f;
+    [SerializeField] private float tileLength = 19.9f;
+    [SerializeField] private int tilesToSpawn = 20;
+
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Tile tile = startTile;
+        for (int i = 0; i < tilesToSpawn; i++)
+        {
+            tile = tile.SpawnNext();
+        }
     }
 
     // Update is called once per frame
