312fd0aec80f19c70539252a9c5d84b80edfe0cd
Samuel Einheri <Samuel.einheri@gmail.com>
2019-08-31 20:43:48 +0200

    Fixed RightEnter_RightExit Tile
    Fixed the scale of the "Mdl_RightEnter_RightExit" level Tile.

--- follow_The_Path/followThePath/Assets/Scripts/scr_CameraFollow.cs
+++ follow_The_Path/followThePath/Assets/Scripts/scr_CameraFollow.cs
@@ -5,7 +5,8 @@ public class scr_CameraFollow : MonoBehaviour
 {
     public Transform target;
     [Range(0.0f, 1f)]
-    public float smoothSpeed = 0.125f;
+    public float smoothSpeed = 0.225f;
+    // 0.125
     public Vector3 offset;
 
     private GameObject playerPrefab;
