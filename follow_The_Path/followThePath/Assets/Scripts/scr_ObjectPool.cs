1ef51b0e1effe463e6d036d6a1c9d90e92b4136a
Samuel Einheri <Samuel.einheri@gmail.com>
2019-09-02 22:05:12 +0200

    Added object pooling scripts if needed
    Added 2 scripts for object pooling if necessary when it needs to be used

---
+++ follow_The_Path/followThePath/Assets/Scripts/scr_ObjectPool.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_ObjectPool : MonoBehaviour
+{
+    scr_PooledObject prefab;
+    public scr_PooledObject GetObject()
+    {
+        scr_PooledObject obj = Instantiate<scr_PooledObject>(prefab);
+        obj.transform.SetParent(transform, false);
+        obj.Pool = this;
+        return obj;
+    }
+
+    public void AddObject (scr_PooledObject o)
+    {
+        Object.Destroy(o.gameObject);
+    }
+}
+/*
+ catlikecoding.com/unity/tutorials/object-pools/
+ www.youtube.com/watch?v=tdSmKaJvCoA
+ */
---
+++ follow_The_Path/followThePath/Assets/Scripts/scr_PooledObject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_PooledObject : MonoBehaviour
+{
+    public scr_ObjectPool Pool { get; set; }
+
+    public void ReturnToPool()
+    {
+        if(Pool)
+        {
+            Pool.AddObject(this);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
+/*
+ catlikecoding.com/unity/tutorials/object-pools/
+ www.youtube.com/watch?v=tdSmKaJvCoA
+ */
