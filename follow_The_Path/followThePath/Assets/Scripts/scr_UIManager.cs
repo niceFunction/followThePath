0ca816511b913ca49965adced55659d8577149e7
Samuel Einheri <Samuel.einheri@gmail.com>
2019-09-13 16:50:38 +0200

    Menu UI redone
    Remade the Main Menu UI and the ingame Pause menu UI. FPS drops for some reason.

---
+++ follow_The_Path/followThePath/Assets/Scripts/scr_UIManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scr_UIManager : MonoBehaviour
+{
+
+    public static bool GameIsPaused = false;
+
+    public void Playgame()
+    {
+        
+        SceneManager.LoadScene("SamuelScene");
+    }
+
+    public void LoadMainMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1;
+        GameIsPaused = false;
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0;
+        GameIsPaused = true;
+    }
+}
