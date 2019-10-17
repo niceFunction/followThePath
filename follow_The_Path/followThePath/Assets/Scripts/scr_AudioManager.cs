ab8bc4b078215cfdfeaad99b01e41109057d7401
Samuel Einheri <Samuel.einheri@gmail.com>
2019-10-17 14:35:46 +0200

    working on musicSlider, not working yet
    Doing work on musicSlider, reference to "onValueChanged" breaks by switching scenes.
    
    - Added new script that will check changed value on music slider

--- follow_The_Path/followThePath/Assets/Scripts/scr_AudioManager.cs
+++ follow_The_Path/followThePath/Assets/Scripts/scr_AudioManager.cs
@@ -1,15 +1,18 @@
 ﻿using UnityEngine.Audio;
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class scr_AudioManager : MonoBehaviour
 {
+
     [Tooltip("Manages sound/volume")]
     public AudioMixer gameMixer;
 
     public Sound[] sounds;
 
     public static scr_AudioManager instance;
+    //public Slider musicSlider;
 
     // Start is called before the first frame update
     void Awake()
@@ -41,7 +44,10 @@ public class scr_AudioManager : MonoBehaviour
 
     void Start()
     {
-        // Play Main Theme here?
+        
+        //musicSlider.value = PlayerPrefs.GetFloat("musicVolume", 1.0f);
+        
+
         PlaySound("MainTheme_01");
     }
 
@@ -57,11 +63,11 @@ public class scr_AudioManager : MonoBehaviour
         s.source.Play();
     }
 
-    public void SetMusicVol (float musicVol)
+    public void SetMusicVol (float sliderValue)
     {
-        //TODO: musicVol_1: While volume can be set, slider isn't saved in the set position when returning to menu
-        //TODO: musicVol_2: When returning to settings, the slider is "full" and the music is still low.
-        gameMixer.SetFloat("musicVolume", 20f * Mathf.Log10(musicVol));
-        Debug.Log(musicVol);
+        gameMixer.SetFloat("musicVolume", Mathf.Log10(sliderValue) * 20);
+        //PlayerPrefs.SetFloat("musicVol", sliderValue);
+        Debug.Log(sliderValue);
+ 
     }
 }
---
+++ follow_The_Path/followThePath/Assets/Scripts/scr_ManageSliderValue.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class scr_ManageSliderValue : MonoBehaviour
+{
+    scr_AudioManager audioManager;
+
+    private Slider musicSlider;
+    /*
+    sliderns OnValueChanged går till det nya scriptet som du lägger på samma objekt.
+    Det scriptet kör: AudioManager.instance.SetMusicVolume eller något
+    Audiomanagern sätter sedan den faktiska volymen.
+
+    i det nya scriptet så lägger du också 
+    Start()
+    { slider.setvalue(AudioManager.instance.GetMusicVolume) }
+    eller liknande
+    så det nya objektet gör bara 2 saker:
+    Vid start, hämta volymen från Audiomanager och sätt slider på rätt plats
+    När slider ändras, skicka vidare det till Audiomanagern
+     */
+    // Start is called before the first frame update
+    void Start()
+    {
+        musicSlider.SetValueWithoutNotify(scr_AudioManager.instance.SetMusicVol);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
