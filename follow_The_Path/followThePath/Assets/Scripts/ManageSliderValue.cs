8e444834cae79f575fd78f6d11cd465dfa7c2dfc
Samuel Einheri <Samuel.einheri@gmail.com>
2019-10-18 19:00:52 +0200

    Slider remembers its position, still a WIP
    Slider now "remembers" its own position depending where the slider is moved.
    The "onValueChanged" reference still breaks between scenes

--- follow_The_Path/followThePath/Assets/Scripts/ManageSliderValue.cs
+++ follow_The_Path/followThePath/Assets/Scripts/ManageSliderValue.cs
@@ -8,12 +8,18 @@ public class ManageSliderValue : MonoBehaviour
 {
     private AudioManager audioManager;
 
+    [SerializeField]
     private Slider musicSlider;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        /* 
+         This "remembers" the positioning of the slider, but the
+         slider still loses the "onValueChanged" reference.
+        */
+        //musicSlider = GameObject.Find("MusicSlider").GetComponent<Slider>();
         musicSlider.value = AudioManager.instance.GetMusicVolume();   
     }
     
