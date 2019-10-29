d1f77971bffae7e22b64008b9bae2c0c6b9ba93b
Samuel Einheri <Samuel.einheri@gmail.com>
2019-10-29 17:36:03 +0100

    Added collision SFX to Tiles
    Added new collision sound effects that plays when the player collides with a Tile.
    - Added new collision SFX
    - Prevented the Player from moving on top of the Tiles

---
+++ follow_The_Path/followThePath/Assets/Scripts/DetectBallCollision.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class DetectBallCollision : MonoBehaviour
+{
+    private AudioSource collisionSource;
+    public AudioClip[] collisionClips;
+
+    private void Start()
+    {
+        collisionSource = GetComponent<AudioSource>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player") 
+        {
+            Debug.Log("Collision Detected");
+            collisionSource.Stop();
+            collisionSource.PlayOneShot(collisionSource.clip = collisionClips[Random.Range(0, collisionClips.Length)]);
+        }
+        
+    }
+}
