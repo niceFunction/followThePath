acfdf6444af3012fc8942b5ba7c88c4c8e93687b
Samuel Einheri <Samuel.einheri@gmail.com>
2019-11-01 16:45:23 +0100

    Added script for collectible
    Added a class for future implementation for creating a collectible and score

---
+++ follow_The_Path/followThePath/Assets/Scripts/Collectible.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+    // SCORE & UI - Brackeys
+    //youtu.be/TAGZxRMloyU
+    private int collectValue = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
