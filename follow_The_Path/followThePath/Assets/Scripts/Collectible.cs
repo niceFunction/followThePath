257b78220f9888c1964fa692630a0866dad6b92b
Samuel Einheri <Samuel.einheri@gmail.com>
2019-11-05 16:51:21 +0100

    Added new SFX to collectible
    Added a new sound effect that plays when the player has collided with the collectible
    
    - Added new SFX to the project
    - Added Tooltips to variables in Editor

--- follow_The_Path/followThePath/Assets/Scripts/Collectible.cs
+++ follow_The_Path/followThePath/Assets/Scripts/Collectible.cs
@@ -6,16 +6,28 @@ public class Collectible : MonoBehaviour
 {
     // SCORE & UI - Brackeys
     //youtu.be/TAGZxRMloyU
-    private int collectValue = 1;
-    private Collider scoreCube;
+    //private int collectValue = 1;
+    //private Collider scoreCube;
+
+    [Tooltip("Destroy the Collectible parent, which this object is a child of.")]
     public GameObject collectibleParent;
     [Range(0.1f, 1.0f)]
     public float destroyDelay = 0.5f;
 
+    /// <summary>
+    /// Get the AudioSource component
+    /// </summary>
+    private AudioSource collectSource;
+    /// <summary>
+    /// The Audio Clip that's supposed to play
+    /// </summary>
+    public AudioClip collectClip;
+
     // Start is called before the first frame update
     void Start()
     {
-        scoreCube = GetComponent<Collider>();
+        //scoreCube = GetComponent<Collider>();
+        collectSource = GetComponent<AudioSource>();
 
     }
 
@@ -27,9 +39,11 @@ public class Collectible : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Player")
+        if (other.gameObject.tag == "Player" && !collectSource.isPlaying)
         {
             Invoke("DestroyObject", destroyDelay);
+            collectSource.Stop();
+            collectSource.PlayOneShot(collectSource.clip = collectClip);
             Debug.Log("Player collided with Collectible");
         }
     }
--- follow_The_Path/followThePath/Assets/Scripts/CollectibleParent.cs
+++ follow_The_Path/followThePath/Assets/Scripts/CollectibleParent.cs
@@ -2,49 +2,55 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Used to rotate the collectible along the Y-axis
+/// </summary>
 public class CollectibleParent : MonoBehaviour
 {
+    [Tooltip("Used to reference itself")]
     [SerializeField]
     private GameObject collectibleParent;
 
+    [Tooltip("Set how fast the Collectible rotates, should be the same as counter-clockwise.")]
     [SerializeField]
     [Range(0.01f, 1.0f)]
     private float clockwiseSpeed = 1.0f;
     
+    [Tooltip("Set how fast the Collectible rotates, should be the same as clockwise.")]
     [SerializeField]
     [Range(-1.0f, -0.01f)]
     private float counterClockwiseSpeed = -1.0f;
+    
+    /// <summary>
+    /// The collectibles current rotation
+    /// </summary>
+    private float currentRotation;
+    /// <summary>
+    /// Used to for choosing a direction
+    /// </summary>
+    private int randomDirection;
 
-    private float currentSpeed;
-
-    private int randomDirection; //= Random.Range(0, 1);
-    private bool isClockwise = false;
-    private bool isCounterClockwise = false;
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
 
     private void Awake()
     {
+        
         randomDirection = Random.Range(0, 2);
         Debug.Log("Rotation direction: " + randomDirection);
         if (randomDirection == 0)
         {
-            currentSpeed = counterClockwiseSpeed;
+            // Set currentRotation to Counter-Clockwise
+            currentRotation = counterClockwiseSpeed;
         }
         else if (randomDirection == 1)
         {
-            currentSpeed = clockwiseSpeed;
+            // Set currentRotation to Clockwise.
+            currentRotation = clockwiseSpeed;
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        collectibleParent.transform.Rotate(0, currentSpeed, 0);
+        collectibleParent.transform.Rotate(0, currentRotation, 0);
     }
 }
