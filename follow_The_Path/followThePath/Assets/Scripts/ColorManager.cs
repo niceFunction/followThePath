a0ebdca59fe3c2e175107d44f6d9b2fe827772e7
Samuel Einheri <Samuel.einheri@gmail.com>
2019-11-18 17:41:46 +0100

    Increased graphics quality in build and Added duplicated material for testing
    Fixed problem with "clipping" shadows in build by increasing quality in Build.
    Made a duplicate of existing material for testing changing colors over time.
    
    - Removed ColorManager object from MainMenu scene and moved it to Test scene instead

--- follow_The_Path/followThePath/Assets/Scripts/ColorManager.cs
+++ follow_The_Path/followThePath/Assets/Scripts/ColorManager.cs
@@ -3,29 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// Manager class used to change the color/emission of prefabs, specifically Tiles and Floors.
+/// Manager class used to change the color of prefabs, specifically Tiles and Floors.
 /// </summary>
 public class ColorManager : MonoBehaviour
 {
 
-    [SerializeField]
-    [Tooltip("Create a list of Colors and choose Colors")]
-    private Color[] colorList;
-
     [Tooltip("Used to change color (Tint) on the Tiles")]
     public Material tileMaterial;
-    [Tooltip("Used to change the color of the Emission on the Floor")]
+    [Tooltip("Used to change the color of the Floor")]
     public Material floorMaterial;
+
+    [Tooltip("How fast will the Color chnage into a new Color?")]
+    public float changeSpeed = 1.0f;
+
+    [SerializeField]
+    [Tooltip("Create a list of Colors and choose Colors for Tiles")]
+    private Color[] tileColorList;
+    [SerializeField]
+    [Tooltip("Create a list of Colors and choose Colors for Floors")]
+    private Color[] floorColorList;
+
+    private float randomNumber;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        tileMaterial.color = GetComponent<Material>().color;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void ColorChange()
+    {
+        //TODO Look more into getting random element from array
+        int tileColorIndex = Random.Range(0, tileColorList.Length);
     }
 }
