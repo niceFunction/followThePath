44785eea049cac48ea8a684be120de090e9c6ff7
Samuel Einheri <Samuel.einheri@gmail.com>
2019-11-19 18:15:30 +0100

    Added more content to ColorManager
    Added more colors to ColorManager prefab for future use and added more content to ColorManager script

--- follow_The_Path/followThePath/Assets/Scripts/ColorManager.cs
+++ follow_The_Path/followThePath/Assets/Scripts/ColorManager.cs
@@ -7,7 +7,6 @@ using UnityEngine;
 /// </summary>
 public class ColorManager : MonoBehaviour
 {
-
     [Tooltip("Used to change color (Tint) on the Tiles")]
     public Material tileMaterial;
     [Tooltip("Used to change the color of the Floor")]
@@ -23,23 +22,58 @@ public class ColorManager : MonoBehaviour
     [Tooltip("Create a list of Colors and choose Colors for Floors")]
     private Color[] floorColorList;
 
-    private float randomNumber;
-
     // Start is called before the first frame update
     void Start()
     {
-        tileMaterial.color = GetComponent<Material>().color;
+        tileMaterial = GetComponent<Renderer>().material;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void ColorChange()
     {
+
         //TODO Look more into getting random element from array
         int tileColorIndex = Random.Range(0, tileColorList.Length);
+
+        if (tileColorIndex == 0)
+        {
+            // Set Tile color to RED
+            tileMaterial.SetColor("_Tint", tileColorList[0]);
+        }
+        else if (tileColorIndex == 1)
+        {
+            // Set Tile color to ORANGE
+            tileMaterial.SetColor("_Tint", tileColorList[1]);
+        }
+        else if (tileColorIndex == 2)
+        {
+            // Set Tile color to YELLOW
+            tileMaterial.SetColor("_Tint", tileColorList[2]);
+        }
+        else if (tileColorIndex == 3)
+        {
+            // Set Tile color to GREEN
+            tileMaterial.SetColor("_Tint", tileColorList[3]);
+        }
+        else if (tileColorIndex == 4)
+        {
+            // Set Tile color to BLUE
+            tileMaterial.SetColor("_Tint", tileColorList[4]);
+        }
+        else if (tileColorIndex == 5)
+        {
+            // Set Tile color to INDIGO
+            tileMaterial.SetColor("_Tint", tileColorList[5]);
+        }
+        else
+        {
+            // Set Tile color to VIOLET
+            tileMaterial.SetColor("_Tint", tileColorList[6]);
+        }
     }
 }
