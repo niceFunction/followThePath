2fffe70df842e9bcac043457447dabafe9b0b55a
Samuel Einheri <Samuel.einheri@gmail.com>
2019-11-20 19:37:12 +0100

    Progress on ColorManager
    Shader now changes color randomly (hopefully)
    - Added another test material to test Floor colors

--- follow_The_Path/followThePath/Assets/Scripts/ColorManager.cs
+++ follow_The_Path/followThePath/Assets/Scripts/ColorManager.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 /// </summary>
 public class ColorManager : MonoBehaviour
 {
+
     [Tooltip("Used to change color (Tint) on the Tiles")]
     public Material tileMaterial;
     [Tooltip("Used to change the color of the Floor")]
@@ -25,13 +26,13 @@ public class ColorManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        tileMaterial = GetComponent<Renderer>().material;
+        //tileMaterial = GetComponent<Renderer>().sharedMaterial;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        ColorChange();
     }
 
     void ColorChange()
@@ -39,41 +40,63 @@ public class ColorManager : MonoBehaviour
 
         //TODO Look more into getting random element from array
         int tileColorIndex = Random.Range(0, tileColorList.Length);
+        int floorColorIndex = Random.Range(0, floorColorList.Length);
 
-        if (tileColorIndex == 0)
+        if (tileColorIndex == 0 && floorColorIndex == 0)
         {
             // Set Tile color to RED
-            tileMaterial.SetColor("_Tint", tileColorList[0]);
+            //tileMaterial.SetColor("_Tint", tileColorList[0]);
+            tileMaterial.color = tileColorList[0];
+            // Set Floor color to RED
+            floorMaterial.color = floorColorList[0];
         }
-        else if (tileColorIndex == 1)
+        else if (tileColorIndex == 1 && floorColorIndex == 1)
         {
             // Set Tile color to ORANGE
-            tileMaterial.SetColor("_Tint", tileColorList[1]);
+            //tileMaterial.SetColor("_Tint", tileColorList[1]);
+            tileMaterial.color = tileColorList[1];
+            // Set Floor color to ORANGE
+            floorMaterial.color = floorColorList[1];
         }
-        else if (tileColorIndex == 2)
+        else if (tileColorIndex == 2 && floorColorIndex == 2)
         {
             // Set Tile color to YELLOW
-            tileMaterial.SetColor("_Tint", tileColorList[2]);
+            //tileMaterial.SetColor("_Tint", tileColorList[2]);
+            tileMaterial.color = tileColorList[2];
+            // Set Floor color to YELLOW
+            floorMaterial.color = floorColorList[2];
         }
-        else if (tileColorIndex == 3)
+        else if (tileColorIndex == 3 && floorColorIndex == 3)
         {
             // Set Tile color to GREEN
-            tileMaterial.SetColor("_Tint", tileColorList[3]);
+            //tileMaterial.SetColor("_Tint", tileColorList[3]);
+            tileMaterial.color = tileColorList[3];
+            // Set Floor color to GREEN
+            floorMaterial.color = floorColorList[3];
         }
-        else if (tileColorIndex == 4)
+        else if (tileColorIndex == 4 && floorColorIndex == 4)
         {
             // Set Tile color to BLUE
-            tileMaterial.SetColor("_Tint", tileColorList[4]);
+            //tileMaterial.SetColor("_Tint", tileColorList[4]);
+            tileMaterial.color = tileColorList[4];
+            // Set Floor color to BLUE
+            floorMaterial.color = floorColorList[4];
         }
-        else if (tileColorIndex == 5)
+        else if (tileColorIndex == 5 && floorColorIndex == 5)
         {
             // Set Tile color to INDIGO
-            tileMaterial.SetColor("_Tint", tileColorList[5]);
+            //tileMaterial.SetColor("_Tint", tileColorList[5]);
+            tileMaterial.color = tileColorList[5];
+            // Set Floor color to INDIGO
+            floorMaterial.color = floorColorList[5];
         }
-        else
+        else if (tileColorIndex == 6 && floorColorIndex == 6)
         {
             // Set Tile color to VIOLET
-            tileMaterial.SetColor("_Tint", tileColorList[6]);
+            //tileMaterial.SetColor("_Tint", tileColorList[6]);
+            tileMaterial.color = tileColorList[6];
+            // Set Floor color to VIOLET
+            floorMaterial.color = floorColorList[6];
         }
     }
 }
