c0be60b3dd8e5900c7b566aac24ff0dfe9e6310d
Samuel Einheri <Samuel.einheri@gmail.com>
2019-11-28 17:50:16 +0100

    Added comments for a solution
    Added comments in ColorManager that will be used for a solution to choosing elements more randomly

--- follow_The_Path/followThePath/Assets/Scripts/ColorManager.cs
+++ follow_The_Path/followThePath/Assets/Scripts/ColorManager.cs
@@ -11,17 +11,24 @@ using UnityEngine;
 
 public class ColorManager : MonoBehaviour
 {
-
-    [Tooltip("Used to change color (Tint) on the Tiles")]
+    /// <summary>
+    /// The Materials are added to the references in the Inspector
+    /// </summary>
+    [Tooltip("Used to change color on the Tiles")]
     public Material tileMaterial;
     [Tooltip("Used to change the color of the Floor")]
     public Material floorMaterial;
 
+    /// <summary>
+    /// The Color arrays size are specified in the Inspector.
+    /// In this case, colors of the Rainbow and the colors for the floor
+    /// is in a darker hue.
+    /// </summary>
     [SerializeField]
-    [Tooltip("Create an Array of Colors and choose Colors for Tiles")]
+    [Tooltip("Creates an Array of Colors for Tiles")]
     private Color[] tileColorList;
     [SerializeField]
-    [Tooltip("Create an Array of Colors and choose Colors for Floors")]
+    [Tooltip("Create an Array of Colors for Floors")]
     private Color[] floorColorList;
 
     // Used to get/set the current Color
@@ -57,7 +64,7 @@ public class ColorManager : MonoBehaviour
         {
             colorChangeTimer = colorChangeTimerReset;
             StartCoroutine(SetRandomColor());
-            Debug.Log("Currently Changing Color: " + currentlyChangingColor);            
+            Debug.Log("Currently Changing Color: " + currentlyChangingColor);
         }
 
     }
@@ -66,12 +73,24 @@ public class ColorManager : MonoBehaviour
     /// This Coroutine is used to randomize the "Tile" colors using Random.Range
     /// </summary>
     /// <returns></returns>
+
+
+    /* Store "tileColorIndex" in a temporary variable that removes elements from it
+     Figure out how to store "tileColorIndex" in a temporary "list" variable and randomly choose from that list
+     and remove that element from that "list".
+     When that temporary "list" is empty, "repopluate" that "list" again.
+
+    https://forum.unity.com/threads/how-to-not-pick-same-object-from-array-twice-in-a-row.298546/
+    https://stackoverflow.com/questions/43708077/how-to-prevent-same-object-from-being-picked-from-array-twice-in-a-row
+    https://www.reddit.com/r/Unity3D/comments/7hp91b/can_i_avoid_repeated_numbers_with_randomrange/?sort=top
+     */
     IEnumerator SetRandomColor()
     {
         currentlyChangingColor = true;
         //TODO Look more into getting random element from array
         int tileColorIndex = Random.Range(0, tileColorList.Length);
         int floorColorIndex = Random.Range(0, floorColorList.Length);
+        Debug.Log("Color Element is: " + tileColorIndex);
 
         float elapsedTime = 0.0f;
         float totalTime = 6.0f;
@@ -83,13 +102,10 @@ public class ColorManager : MonoBehaviour
             float fraction = Mathf.Sin(elapsedTime / totalTime);
 
             tileMaterial.color = Color.Lerp(currentTileColor, tileColorList[tileColorIndex], fraction);
-            floorMaterial.color = Color.Lerp(currentFloorColor, floorColorList[tileColorIndex], fraction);
-            /*
-               floorColorIndex was used before, line of code commented to remember
-               floorMaterial.color = Color.Lerp(currentFloorColor, floorColorList[floorColorIndex], fraction);
-            */
+            floorMaterial.color = Color.Lerp(currentFloorColor, floorColorList[floorColorIndex], fraction);
+
             yield return null;
-            Debug.Log("Current Color Index: " + tileColorIndex);
+            //Debug.Log("Current Color Index: " + tileColorIndex);
 
         }
         currentlyChangingColor = false;
