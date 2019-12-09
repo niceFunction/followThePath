8a31c2f0db39e180fd7288dd8ae28d33c1d83c47
Samuel Einheri <Samuel.einheri@gmail.com>
2019-12-09 19:47:55 +0100

    Added a comment for a reference

--- follow_The_Path/followThePath/Assets/Scripts/ChangeFont.cs
+++ follow_The_Path/followThePath/Assets/Scripts/ChangeFont.cs
@@ -10,7 +10,7 @@ public class ChangeFont : MonoBehaviour
     // currentFont of the type TextMeshPro
     private ColorManager colorManager;
 
-
+    // https://forum.unity.com/threads/trouble-programatically-changing-text-mesh-pro-font.470047/
     // Start is called before the first frame update
     void Start()
     {
