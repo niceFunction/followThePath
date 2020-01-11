57fe56d3d3d45cddb467338cb38e88cd2eca4714
Samuel Einheri <Samuel.einheri@gmail.com>
2020-01-11 17:28:48 +0100

    Added unfinished variable to class
    Added a to be used variable in "DelegateAndEvent".

--- follow_The_Path/followThePath/Assets/Scripts/DelegateAndEvent.cs
+++ follow_The_Path/followThePath/Assets/Scripts/DelegateAndEvent.cs
@@ -1,8 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class DelegateAndEvent : MonoBehaviour
 {
-    
+    //public delegate void UxEventHandler;
+
 }
