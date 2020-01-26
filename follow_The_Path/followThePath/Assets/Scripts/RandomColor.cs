71d1b9dcdb6e8e6278e068d9a14262a0b72b740b
Samuel Einheri <Samuel.einheri@gmail.com>
2020-01-26 21:20:48 +0100

    Added new foundation for new color randomisation
    Added couple of new variables and methods (with content in them) into the temporary color randomisation class

--- follow_The_Path/followThePath/Assets/Scripts/RandomColor.cs
+++ follow_The_Path/followThePath/Assets/Scripts/RandomColor.cs
@@ -18,20 +18,36 @@ public class RandomColor : MonoBehaviour
     [SerializeField, Tooltip("List of colors for the second material")]
     private Color[] randomColorListTwo; // This variable will be replaced by "floorMaterial"
 
-    private Color currentRandomColorOne;
-    private Color currentRandomColorTwo;
+    //private Color currentRandomColorOne;
+    //private Color currentRandomColorTwo;
+
+    private Color currentTileColor;
+    private Color currentFloorColor;
+
+    private Color previousTileColor;
+    private Color previousFloorColor;
 
     private Color colorA;
     private Color colorB; 
 
+    [SerializeField]
     private float changeColorTimeAmount;
     public float changeColorTimeReset;
     private bool isChangingColor;
 
+    List<Color> tileColors = new List<Color>();
+    List<Color> floorColors = new List<Color>();
+
+    bool hasSetFirstColor = false;
+
     // Start is called before the first frame update
     void Start()
     {
+
+        tileColors.AddRange(randomColorListOne);
+        floorColors.AddRange(randomColorListTwo);
         changeColorTimeAmount = changeColorTimeReset;
+        StartCoroutine(MakeRandomColor());
     }
 
     // Update is called once per frame
@@ -43,7 +59,7 @@ public class RandomColor : MonoBehaviour
 
     private void ChangeNewRandomColor()
     {
-        changeColorTimeAmount -= Time.deltaTime;
+        /* changeColorTimeAmount -= Time.deltaTime;
 
         if (changeColorTimeAmount < 0)
         {
@@ -52,18 +68,46 @@ public class RandomColor : MonoBehaviour
             // Start the Coroutine here
             StartCoroutine(MakeRandomColor());
         }
+
+
+        Color tempTileColor = null;
+        if (currentTileColor != null)
+        {
+            tempTileColor = previousTileColor;
+        }
+
+        currentTileColor = tileColors(Random.Range(0, randomColorListOne.Length));
+        
+
+        if (tempTileColor != null)
+        {
+            // Put the temp color back into the list
+        }
+        */
+        Color tempTileColor = previousTileColor;
+        int index = Random.Range(0, randomColorListOne.Length);
+        currentTileColor = randomColorListOne[index];
+
+        tileColors.RemoveAt(index);
+        if (hasSetFirstColor)
+        {
+            tileColors.Add(tempTileColor);
+            hasSetFirstColor = true;
+        }
     }
 
     private void SetColorValue()
     {
+
+        // TODO Calculate the new exact color to apply to the material, using the material and currentFloorColor
+
         //int colorIndexA = Random.Range(0, randomColorListOne.Length);
         //int colorA = randomColorListOne[i];
 
         //int colorIndexA = Random.Range(0, randomColorListOne.Length);
         //Color colorA = randomColorListOne[colorIndexA];
-
     }
-    
+
     IEnumerator MakeRandomColor()
     {
         /*
@@ -76,7 +120,21 @@ public class RandomColor : MonoBehaviour
             float fraction = Mathf.Sin(elapsedTime / totalTime);
         }
         */
+        float elapsedTime = 0f;
+        float totalTime = changeColorTimeAmount;
         
+        while(true)
+        {
+            ChangeNewRandomColor(); // Select the new Color
+            while(elapsedTime < totalTime)
+            {
+                // This loop makes sure the color is updated
+                elapsedTime += Time.deltaTime;
+                float fraction = Mathf.Sin(elapsedTime / totalTime);
+                //SetColorValue(fraction);
+            }
+            //SetColorValue(1f);
+        }
     }
     
 }
