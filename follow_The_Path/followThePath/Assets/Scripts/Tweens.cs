7423006707ee8ee1c8bbcc7ab2ad0d4b699ab0a0
Samuel Einheri <Samuel.einheri@gmail.com>
2020-05-15 13:10:45 +0200

    Added additional structs for specific menu objects
    Added 3 additional structs  for object found in the Main Menu, the objects are:
    - Main Menu
    - How To Play Men
    - Settings Menu

--- follow_The_Path/followThePath/Assets/Scripts/Tweens.cs
+++ follow_The_Path/followThePath/Assets/Scripts/Tweens.cs
@@ -23,4 +23,61 @@ public class Tweens : MonoBehaviour
         [Tooltip("Scale of object"), Range(0, 1)]
         public float Scale;
     }
+
+    [System.Serializable]
+    public struct MainMenu
+    {
+        [Tooltip("The RectTransform of the Main Menu object")]
+        public RectTransform TweenTarget;
+
+        [Tooltip("The amount of time for the tween of the Main Menu to happen"), Range(0.01f, 2f)]
+        public float TweenTime;
+
+        [Tooltip("The final alpha the background image will have for the Main Menu"), Range(0.0f, 1f)]
+        public float Alpha;
+
+        [Tooltip("The X-position the Main Menu will have"), Range(-5000, 5000)]
+        public int XPosition;
+
+        [Tooltip("The Y-position the Main Menu will have"), Range(-5000, 5000)]
+        public int YPosition;
+    }
+
+    [System.Serializable]
+    public struct HowToPlayMenu
+    {
+        [Tooltip("The RectTransform of the How To Play Menu object")]
+        public RectTransform TweenTarget;
+
+        [Tooltip("The amount of time for the tween of the How To Play Menu to happen"), Range(0.01f, 2f)]
+        public float TweenTime;
+
+        [Tooltip("The final alpha the background image will have for the How To Play Menu"), Range(0.0f, 1f)]
+        public float Alpha;
+
+        [Tooltip("The X-position the How To Play Menu will have"), Range(-5000, 5000)]
+        public int XPosition;
+
+        [Tooltip("The Y-position the How To Play Menu will have"), Range(-5000, 5000)]
+        public int YPosition;
+    }
+
+    [System.Serializable]
+    public struct SettingsMenu
+    {
+        [Tooltip("The RectTransform of the Settings Menu object")]
+        public RectTransform TweenTarget;
+
+        [Tooltip("The amount of time for the tween of the Settings Menu to happen"), Range(0.01f, 2f)]
+        public float TweenTime;
+
+        [Tooltip("The final alpha the background image will have for the Settings Menu"), Range(0.0f, 1f)]
+        public float Alpha;
+
+        [Tooltip("The X-position the Settings Menu will have"), Range(-5000, 5000)]
+        public int XPosition;
+
+        [Tooltip("The Y-position the Settings Menu will have"), Range(-5000, 5000)]
+        public int YPosition;
+    }
 }
