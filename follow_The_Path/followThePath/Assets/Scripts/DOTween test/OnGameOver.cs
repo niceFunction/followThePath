a0b5010944617cb5502084c707cf5a7805b521b8
Samuel Einheri <Samuel.einheri@gmail.com>
2020-07-09 22:38:40 +0200

    Updated Unity version, added variables to "OnGameOver" script

--- follow_The_Path/followThePath/Assets/Scripts/DOTween test/OnGameOver.cs
+++ follow_The_Path/followThePath/Assets/Scripts/DOTween test/OnGameOver.cs
@@ -8,7 +8,7 @@ public class OnGameOver : MonoBehaviour
     //TODO GameOver 1: look into breaking things out from "GameManager" that's related to "Game Over"
     //TODO GameOver 2: And call(?) those variables in "OnGameOver" from "GameManger" instead (maybe?)
 
-    [SerializeField, Tooltip("Game Over Menu transform"), Header("Visual")]
+    [SerializeField, Tooltip("Game Over Menu transform (MAY NOT BE NEEDED)"), Header("Visual")]
     private Transform gameOverTransform;
     public Transform GameOverTransform { get { return gameOverTransform; } }
 
@@ -16,6 +16,32 @@ public class OnGameOver : MonoBehaviour
     private Image backgroundImage;
     public Image BackgroundImage { get { return backgroundImage; } }
 
+    [SerializeField, Tooltip("Game Over Menu object"), Header("GameObjects")]
+    private GameObject gameOverMenuObject;
+    public GameObject GameOverMenuObject { get { return gameOverMenuObject; } }
+
+    [SerializeField, Tooltip("GameObject that starts counting down")]
+    private GameObject gameOverTimerObject;
+    public GameObject GameOverTimerObject { get { return gameOverTimerObject; } }
+
+    [SerializeField, Tooltip("Amount of time needed to trigger the Game Over timer"), Range(1, 60), Header("Timers")]
+    private float backgroundTimer = 5.0f;
+    public float BackgroundTimer { get { return backgroundTimer; } }
+    
+    [SerializeField, Tooltip("Sets the 'setBackgroundTimer' to 'backgroundTimer', (USED FOR TESTING)")]
+    private float setBackgroundTimer;
+    public float SetBackgroundTimer { get { return setBackgroundTimer; } }
+
+    [SerializeField, Tooltip("Amount of time until 'Game Over' (will show up on screen)"), Range(1, 60)]
+    private float gameOverTimer = 5.0f;
+    public float GameOverTimer { get { return gameOverTimer; } }
+    // Resets the 'Game Over' timer
+    private float resetGameOverTimer;
+    public float ResetGameOverTimer { get { return resetGameOverTimer; } }
+
+    [SerializeField, Tooltip("Minimum amount of speed to trigger the backgroundTimer"), Range(0.01f, 5.0f), Header("Minimum Speed")]
+    private float minimumSpeed = 1.0f;
+    public float MinimumSpeed { get { return minimumSpeed; } }
     // Start is called before the first frame update
     void Start()
     {
