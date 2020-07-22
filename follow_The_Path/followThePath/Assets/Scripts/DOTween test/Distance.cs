7896eee5d50aef63edd1240be46f8afed7ae261a
Samuel Einheri <Samuel.einheri@gmail.com>
2020-07-22 19:36:09 +0200

    Distance value now only increases if the Ball moves further along the Z-axis
    Distance "score" value will now only increase if the Ball is moving further on the Z-axis, if it doesn't move forwards, the distance "score" value stops

--- follow_The_Path/followThePath/Assets/Scripts/DOTween test/Distance.cs
+++ follow_The_Path/followThePath/Assets/Scripts/DOTween test/Distance.cs
@@ -8,13 +8,7 @@ public class Distance : MonoBehaviour
 {
     /*
      Points by distance:
-    Unity Manual: https://docs.unity3d.com/Manual/DirectionDistanceFromOneObjectToAnother.html
-    StackOverflow Scoring by distance: https://stackoverflow.com/questions/53176261/c-sharp-unity-scoring-system-by-distance
     distance based scoring system: https://answers.unity.com/questions/923146/distance-based-score-system.html
-    Associate time & distance for a score system: https://answers.unity.com/questions/1120708/associate-time-and-distance-for-a-score-system.html
-    Reddit Z-axis increases by 500: https://www.reddit.com/r/Unity3D/comments/ddn4et/how_can_i_create_a_very_simple_point_scoring/?sort=top
-    how to make the number of points equal to distance traveled: https://answers.unity.com/questions/1505643/how-to-make-the-number-of-points-equal-to-the-dist.html
-
     */
 
     // NOTE: This class should (probably) work together with "GameOver" class
@@ -23,6 +17,8 @@ public class Distance : MonoBehaviour
 
     // The Players distance value
     private float playerDistance;
+    // Player's last "known" position
+    private float playerLastPosition;
 
     [SerializeField, Tooltip("Shows the Players (upper left corner) current distance")]
     private TextMeshProUGUI currentPlayerDistance;
@@ -35,26 +31,11 @@ public class Distance : MonoBehaviour
     [SerializeField, Tooltip("On Game Over, shows the previous final distance")]
     private TextMeshProUGUI previousPlayerDistance; // Shown at the same time as the final distance, shows on bottom
     public TextMeshProUGUI PreviousPlayerDistance { get { return previousPlayerDistance; } }
-    
-    /*
-    [SerializeField, Tooltip("Shows the Player's score")]
-    private TextMeshProUGUI playerHighscore;
-    public TextMeshProUGUI PlayerHighscore { get { return playerHighscore; } }
-
-    [SerializeField, Tooltip("currentHighscoreText displays while game is active")]
-    private TextMeshProUGUI currentHighscore;
-    public TextMeshProUGUI CurrentHighscore { get { return currentHighscore; } }
-
-    [SerializeField, Tooltip("Displays the highscore on Game Over")]
-    private TextMeshProUGUI gameOverHighscore;
-    public TextMeshProUGUI GameOverHighscore { get { return gameOverHighscore; } }
-    */
 
     public static Distance Instance { get; private set; }
 
-    //TODO Distance 1: Distance is increased when rolling along the Z-axis
-    //TODO Distance 2: Distance only increases whenever player is rolling FORWARDS & never backwards
-    //TODO Distance 3: Distance will be measured in Meters but as a "Setting", can be changed to Imperial instead
+    //TODO Distance 3: (Has to be done laster) Distance will be measured in Meters but as a "Setting", can be changed to Imperial instead
+    // NOTE: Consider using a Button slider for it with an INT, 0 = OFF & 1 = ON
 
     private void Awake()
     {
@@ -72,6 +53,9 @@ public class Distance : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // What is the Ball's last position on Z-axis
+        playerLastPosition = ball.transform.position.z;
+
         // NOTE: Get this variable/function in their own method?
         currentPlayerDistance.text = PlayerPrefs.GetFloat("Distance", 0).ToString();
     }
@@ -79,7 +63,7 @@ public class Distance : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //MeasureDistance();
+
     }
 
     /// <summary>
@@ -87,15 +71,16 @@ public class Distance : MonoBehaviour
     /// </summary>
     public void MeasureDistance()
     {
-        // NOTE: In here, prevent the "MeasureDistance" from going down & only go up
-        /* NOTE: ensure that the ball is "increasing" in value
-        if(ball.RB.velocity.z)
+        // NOTE 1: Ball's value is now only increasing aslong as it moves further along Z-axis
+        // NOTE 2: however, its updating "playerLastPosition" every frame(?), can that be "healthy"?
+
+        // Increases the distance as long as the Ball is moving further on the Z-axis
+        if(ball.transform.position.z > playerLastPosition)
         {
-
+            playerLastPosition = ball.transform.position.z;
+            playerDistance = Vector3.Distance(ball.transform.position, transform.position);
         }
-        */
-        playerDistance = Vector3.Distance(ball.transform.position, transform.position);
-
+        
         currentPlayerDistance.text = playerDistance.ToString("F1");
 
     }
