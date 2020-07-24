764b4c6e6a339ee57220c5aedf30c25ac1bdb1d8
Samuel Einheri <Samuel.einheri@gmail.com>
2020-07-24 20:25:06 +0200

    Created prefab of "DistanceCalculator" object & renamed "GameOver DOTween" to "Test_Game"

---
+++ follow_The_Path/followThePath/Assets/Scripts/DOTween test/LoadMainMenu_test.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadMainMenu_test : MonoBehaviour
+{
+    [SerializeField]
+    private string testMainMenuSceneName;
+
+    public static LoadMainMenu_test Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void LoadTestMainMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(testMainMenuSceneName);
+    }
+
+}
