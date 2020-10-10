9145560368f9351935f36661464134059411691e
Samuel Einheri <Samuel.einheri@gmail.com>
2020-10-10 16:44:40 +0200

    Added gameObject/script that clears playerprefs history

---
+++ follow_The_Path/followThePath/Assets/Scripts/DebugClear.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace SamuelEinheri
+{
+    public class DebugClear : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void ClearDistanceScore()
+        {
+            Debug.Log("Clearing highscore");
+            PlayerPrefs.DeleteKey("DistanceScore");
+            PlayerPrefs.Save();
+        }
+    }
+}
